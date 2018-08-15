e901513611c52e50971156a3f0b621ad090f28da
root <root@test.com>
2018-08-15 17:43:25 +0800

    deal warring

--- src/com/hy/ly/servlet/UserServlet.java
+++ src/com/hy/ly/servlet/UserServlet.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.util.codec.binary.StringUtils;
-
 import com.alibaba.fastjson.JSON;
 
 @WebServlet("/userServlet")
